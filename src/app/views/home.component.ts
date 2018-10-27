91376cb1a073f0e4e8e248c02576bdcac13411c8
HiCooper <294237781@qq.com>
2018-10-27 21:28:13 +0800

    first-finish

--- src/app/views/home.component.ts
+++ src/app/views/home.component.ts
@@ -10,6 +10,51 @@ import {en_US, NzI18nService, zh_CN} from 'ng-zorro-antd';
 })
 export class HomeComponent implements OnInit {
 
+    featureList = [
+        {
+            imgUrl: '../../assets/img/intro.svg',
+            title: 'Introduction',
+            desc: 'Install from scratch or based on ngx-admin'
+        }, {
+            imgUrl: '../../assets/img/guides.svg',
+            title: 'Guides',
+            desc: 'Theme System configuration, customisation and other articles'
+        }, {
+            imgUrl: '../../assets/img/components.svg',
+            title: 'Components',
+            desc: 'Native Angular components with configurable styles'
+        }, {
+            imgUrl: '../../assets/img/themes.svg',
+            title: 'Theme System',
+            desc: 'Three built-in themes & hundreds of variables to create your own.With hot-reload out of the box'
+        }, {
+            imgUrl: '../../assets/img/auth.svg',
+            title: 'Auth',
+            desc: 'Authentication layer with configurable Strategies'
+        }, {
+            imgUrl: '../../assets/img/security.svg',
+            title: 'Security',
+            desc: 'ACL list with helpful directives'
+        },
+    ];
+
+    advantageList = [
+        {
+            title: 'Modular',
+            desc: 'Each feature is a separate npm module. Use only what you need.'
+        }, {
+            title: 'Native',
+            desc: 'Components are written in pure Angular with no 3rd-party dependencies.'
+        },
+        {
+            title: 'Open',
+            desc: 'Modules source code is free and available under MIT licence.',
+        },
+        {
+            title: 'Extendable',
+            desc: 'Can be used in a mix with any UI library.',
+        }
+    ];
 
     constructor(private router: Router, private analytics: AnalyticsService, private i18n: NzI18nService) {
         this.currentRouterUrl = router.url;
